b7ae613c2fd316d5894483e2cd2e8c96e8707830
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2022-04-04 03:25:13 +0600

    Added all necessary components, corrected routing, made header with routerLinks

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+import { CoursesComponent } from './courses/courses.component';
+import { InfoComponent } from './info/info.component';
+import { LogRegComponent } from './log-reg/log-reg.component';
 import { PublicOfferComponent } from './public-offer/public-offer.component';
+import { TutorsComponent } from './tutors/tutors.component';
 
 const routes: Routes = [
-  { path: 'PublicOffer', component: PublicOfferComponent },
+  { path: 'authorize', component: LogRegComponent },
+  { path: 'authorize/publicoffer', component: PublicOfferComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'tutors', component: TutorsComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'contacts', component: ContactsComponent },
+
+  
+  
 ];
 
 @NgModule({
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicOfferComponent } from './public-offer/public-offer.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { InfoComponent } from './info/info.component';
+import { TutorsComponent } from './tutors/tutors.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LogRegComponent } from './log-reg/log-reg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PublicOfferComponent
+    PublicOfferComponent,
+    HomeComponent,
+    CoursesComponent,
+    CourseDetailComponent,
+    InfoComponent,
+    TutorsComponent,
+    ContactsComponent,
+    LogRegComponent
   ],
   imports: [
     BrowserModule,
