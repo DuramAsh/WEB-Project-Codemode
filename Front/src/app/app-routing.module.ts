a477011701c22f9b16337e33d26a32065b3e2491
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2022-04-05 14:42:59 +0600

    Made some design and positioning, models for Home, Contacts pages

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -6,15 +6,17 @@ import { InfoComponent } from './info/info.component';
 import { LogRegComponent } from './log-reg/log-reg.component';
 import { PublicOfferComponent } from './public-offer/public-offer.component';
 import { TutorsComponent } from './tutors/tutors.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'authorize', component: LogRegComponent },
   { path: 'authorize/publicoffer', component: PublicOfferComponent },
   { path: 'courses', component: CoursesComponent },
   { path: 'tutors', component: TutorsComponent },
   { path: 'info', component: InfoComponent },
   { path: 'contacts', component: ContactsComponent },
-
+  
   
   
 ];
